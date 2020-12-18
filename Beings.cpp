11f3d36a5f8f01b43a89b6e3f7dbffbc96ee9089
Bharadwaj Gorthy <bharathsharma95@gmail.com>
2020-12-18 17:33:15 -0600

    Added Base Class files to continue development

---
+++ Beings.cpp
@@ -0,0 +1,1 @@
+#include "Beings.h"
---
+++ Beings.h
@@ -0,0 +1,15 @@
+#ifndef _Beings_H
+#define _Beings_H
+
+namespace Beings
+{
+  class Base
+  {
+  public:
+    //member functions go here
+  private:
+    //member variables go here
+  };
+}
+
+#endif
